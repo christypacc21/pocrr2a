fa2cc9a1c81e0f66e0399a8382c0162f93117e6d
christy law <christy.law@accedo.tv>
2019-04-29 15:33:28 +0800

    Initial commit- ReactRedux - get data

---
+++ src/actions/items.js
@@ -0,0 +1,42 @@
+// Redux Thunk. Thunk allows you to write action creators that return a function instead of an action. 
+// The inner function can receive the store methods dispatch and getState as parameters, but we'll just use dispatch.
+
+export function itemsHasErrored(bool) {
+  return {
+    type: 'ITEMS_HAS_ERRORED',
+    hasErrored: bool // not use 'payload' here (?) means can customise word? (why do this)where will we retrieve these name as payload?
+  }
+}
+
+export function itemsIsLoading(bool) {
+  return {
+    type: 'ITEMS_IS_LOADING',
+    isLoading: bool
+  }
+}
+
+export function itemsFetchDataSuccess(items) {
+  return {
+    type: 'ITEMS_FETCH_DATA_SUCCESS',
+    items
+  }
+}
+
+
+// Give a star to this redux thunk usage - learn how to do async actions using 'dispatch'
+export function itemsFetchData(url) {
+  return ((dispatch) => {
+    dispatch(itemsIsLoading(true));
+
+    fetch(url)
+     .then(res => {
+       if(!res.ok) {
+         throw Error(res.statusText);
+       }
+       dispatch(itemsIsLoading(false))
+       return res.json();
+     })
+     .then(items => dispatch(itemsFetchDataSuccess(items)))
+     .catch(() => dispatch(itemsHasErrored(true)))
+  })
+}
---
+++ src/components/ItemList.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { itemsFetchData } from '../actions/items';
+
+
+class ItemList extends Component {
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         items: [],
+    //         hasErrored: false,
+    //         isLoading: false
+    //     };
+    // }
+    
+    // fetchData(url) {
+    //   this.setState({isLoading: true});
+    //   fetch(url)
+    //     .then(res => {
+    //       if(!res.ok) {
+    //         throw Error(res.statusText);
+    //       }
+    //       this.setState({isLoading: false})
+    //       return res;
+    //     })
+    //    .then(res => res.json())
+    //    .then(items => {
+    //      this.setState({items, isLoading: false})
+    //     //  return res.status(400).json({res})
+    //     })
+    //    .catch(() => this.setState({hasErrored:true}));
+    // }
+
+    componentDidMount() {
+      this.props.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
+    }
+
+    render() {
+        if (this.props.hasErrored) {
+            return <p>Sorry! There was an error loading the items</p>;
+        }
+        if (this.props.isLoading) {
+            return <p>Loading…</p>;
+        }
+        return (
+            <ul>
+                {this.props.items.map((item) => (
+                    <li key={item.id}>
+                        {item.label}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    items: state.items,
+    isLoading: state.isLoading,
+    hasErrored: state.hasErrored
+  }
+}
+
+const mapDispatchToProps = ((dispatch) => {
+  return {
+    fetchData: (url) => dispatch(itemsFetchData(url))
+  }
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(ItemList);
