81467d03076638b4004390f132e4e9ededeb64c9
christy law <christy.law@accedo.tv>
2019-04-30 16:41:48 +0800

    redux - better coding style - use variables instead of strings

--- src/actions/items.js
+++ src/actions/items.js
@@ -1,49 +1,54 @@
-// Redux Thunk. Thunk allows you to write action creators that return a function instead of an action. 
+// Redux Thunk. Thunk allows you to write action creators that return a function instead of an action.
 // The inner function can receive the store methods dispatch and getState as parameters, but we'll just use dispatch.
+import {
+  ITEMS_HAS_ERRORED,
+  ITEMS_IS_LOADING,
+  ITEMS_FETCH_DATA_SUCCESS,
+  ITEMS_DELETE_DATA_SUCCESS
+} from "./type";
 
 export function itemsHasErrored(bool) {
   return {
-    type: 'ITEMS_HAS_ERRORED',
+    type: ITEMS_HAS_ERRORED,
     hasErrored: bool // not use 'payload' here (?) means can customise word? (why do this)where will we retrieve these name as payload?
-  }
+  };
 }
 
 export function itemsIsLoading(bool) {
   return {
-    type: 'ITEMS_IS_LOADING',
+    type: ITEMS_IS_LOADING,
     isLoading: bool
-  }
+  };
 }
 
 export function itemsFetchDataSuccess(items) {
   return {
-    type: 'ITEMS_FETCH_DATA_SUCCESS',
+    type: ITEMS_FETCH_DATA_SUCCESS,
     items
-  }
+  };
 }
-
 
 // Give a star to this redux thunk usage - learn how to do async actions using 'dispatch'
 export function itemsFetchData(url) {
-  return ((dispatch) => {
+  return dispatch => {
     dispatch(itemsIsLoading(true));
 
     fetch(url)
-     .then(res => {
-       if(!res.ok) {
-         throw Error(res.statusText);
-       }
-       dispatch(itemsIsLoading(false))
-       return res.json();
-     })
-     .then(items => dispatch(itemsFetchDataSuccess(items)))
-     .catch(() => dispatch(itemsHasErrored(true)))
-  })
+      .then(res => {
+        if (!res.ok) {
+          throw Error(res.statusText);
+        }
+        dispatch(itemsIsLoading(false));
+        return res.json();
+      })
+      .then(items => dispatch(itemsFetchDataSuccess(items)))
+      .catch(() => dispatch(itemsHasErrored(true)));
+  };
 }
 
 export function itemsDeleteData(deletedItem) {
-  return{
-    type: 'ITEMS_DELETE_DATA_SUCCESS',
+  return {
+    type: ITEMS_DELETE_DATA_SUCCESS,
     deletedItem
-  }
+  };
 }
--- src/reducers/items.js
+++ src/reducers/items.js
@@ -1,16 +1,23 @@
-export function itemsHasErrored (state = false, action) {
-  switch(action.type) {
-    case 'ITEMS_HAS_ERRORED':
+import {
+  ITEMS_HAS_ERRORED,
+  ITEMS_IS_LOADING,
+  ITEMS_FETCH_DATA_SUCCESS,
+  ITEMS_DELETE_DATA_SUCCESS
+} from "./type";
+
+export function itemsHasErrored(state = false, action) {
+  switch (action.type) {
+    case ITEMS_HAS_ERRORED:
       return action.hasErrored;
-    
-    default: 
+
+    default:
       return state;
   }
 }
 
 export function itemsIsLoading(state = false, action) {
   switch (action.type) {
-    case 'ITEMS_IS_LOADING':
+    case ITEMS_IS_LOADING:
       return action.isLoading;
 
     default:
@@ -19,13 +26,16 @@ export function itemsIsLoading(state = false, action) {
 }
 
 export function items(state = [], action) {
-  switch(action.type) {
-    case 'ITEMS_FETCH_DATA_SUCCESS':
+  switch (action.type) {
+    case ITEMS_FETCH_DATA_SUCCESS:
       return action.items;
-    
-    case 'ITEMS_DELETE_DATA_SUCCESS':
-      return [...state.slice(0, action.deletedItem), ...state.slice(action.deletedItem + 1)];
-      //state.splice(action.deletedItem,1)
+
+    case ITEMS_DELETE_DATA_SUCCESS:
+      return [
+        ...state.slice(0, action.deletedItem),
+        ...state.slice(action.deletedItem + 1)
+      ];
+    //state.splice(action.deletedItem,1)
 
     default:
       return state;
